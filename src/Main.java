a9d506a48f6ca35c9d50f9ceb0d407a0f2a9691c
Marsikol <122033008+Marsikol@users.noreply.github.com>
2023-01-12 11:43:12 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Ведите своё имя ");
+            String name = input.next();
+
+
+            System.out.println("здздравствуйте ; "+name);
+
+        String myName = "Marsel";
+        String myage = "15";
+        System.out.println(myName + " " + myage);
+
+        String word = "Histori";
+        System.out.println(word);
+
+        final int  IGROKI = 70;
+        final String MONDAY ="Monday";
+
+        
+    }
+}
+
